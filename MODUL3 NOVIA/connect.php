c8498f88d7384d91a5501927e9067b72a5f26cd5
sekaradji <105697602+sekaradji@users.noreply.github.com>
2023-11-17 21:29:31 +0700

    MODUL3

---
+++ MODUL3 NOVIA/connect.php
@@ -0,0 +1,21 @@
+<!-- File ini berisi koneksi dengan database yang telah di import ke phpMyAdmin kalian -->
+
+
+<?php
+// Buatlah variable untuk connect ke database yang telah di import ke phpMyAdmin
+	
+$host = "localhost:3308"; 
+$username = "root"; 
+$password = ""; 
+$database = "modul3_novia"; 
+
+$connect = mysqli_connect($host, $username, $password, $database);
+
+// 
+  
+// Buatlah perkondisian jika tidak bisa terkoneksi ke database maka akan mengeluarkan errornya
+	if(!$connect) {
+		die("Koneksi Gagal Terhubung".mysqli_connect_error());
+	}
+//
+?>
---
+++ MODUL3 NOVIA/list_mobil.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>List Mobil</title>
+    <style>
+        table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 20px;
+        }
+        th, td {
+            border: 1px solid #ddd;
+            padding: 8px;
+            text-align: left;
+        }
+        th {
+            background-color: #f2f2f2;
+        }
+    </style>
+</head>
+<body>
+    <?php include("navbar.php") ?>
+    <center>
+        <div class="container">
+            <h1>List Mobil</h1>
+
+            <?php
+            include("connect.php");
+
+            // Buatlah query untuk mengambil data dari database (gunakan query SELECT)
+            $query = "SELECT * FROM showroom_mobil";
+            $result = mysqli_query($connect, $query);
+            
+            // Buatlah perkondisian dimana: 
+            // 1. a. Apabila ada data dalam database, maka outputnya adalah semua data dalam database akan ditampilkan dalam bentuk tabel 
+            //       (gunakan num_rows > 0, while, dan mysqli_fetch_assoc())
+            //    b. Untuk setiap data yang ditampilkan, buatlah sebuah button atau link yang akan mengarahkan web ke halaman 
+            //       form_detail_mobil.php dimana halaman tersebut akan menunjukkan seluruh data dari satu mobil berdasarkan id
+            // 2. Apabila tidak ada data dalam database, maka outputnya adalah pesan 'tidak ada data dalam tabel'
+
+            //<!--  **********************  1  **************************     -->
+            if (mysqli_num_rows($result) > 0) {
+                echo "<table>";
+                echo "<tr><th>ID</th><th>Nama Mobil</th><th>Brand Mobil</th><th>Warna Mobil</th><th>Tipe Mobil</th><th>Harga Mobil</th><th>Detail</th></tr>";
+
+                while ($row = mysqli_fetch_assoc($result)) {
+                    echo "<tr>";
+                    echo "<td>{$row['id']}</td>";
+                    echo "<td>{$row['nama_mobil']}</td>";
+                    echo "<td>{$row['brand_mobil']}</td>";
+                    echo "<td>{$row['warna_mobil']}</td>";
+                    echo "<td>{$row['tipe_mobil']}</td>";
+                    echo "<td>{$row['harga_mobil']}</td>";
+                    echo "<td><a href='form_detail_mobil.php?id={$row['id']}'>Detail</a></td>";
+                    echo "</tr>";
+                }
+            //<!--  **********************  1  **************************     -->
+
+            //<!--  **********************  2  **************************     -->
+            echo "</table>";
+            } else {
+                echo "Tidak ada data dalam tabel.";
+            }
+            
+            //<!--  **********************  2  **************************     -->
+            ?>
+        </div>
+    </center>
+</body>
+</html>
+
+
