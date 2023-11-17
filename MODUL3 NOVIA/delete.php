b3769e00b62909f54077112a39c34079d8361579
sekaradji <105697602+sekaradji@users.noreply.github.com>
2023-11-17 21:28:09 +0700

    Add files via upload

---
+++ MODUL3 NOVIA/delete.php
@@ -0,0 +1,21 @@
+<!-- Pada file ini kalian membuat coding untuk logika delete / menghapus data mobil pada showroom sesuai id-->
+<?php 
+// (1) Jangan lupa sertakan koneksi database dari yang sudah kalian buat yaa
+include('connect.php');
+// (2) Tangkap nilai "id" mobil (CLUE: gunakan GET)
+if (isset($_GET['id'])) {
+$id = $_GET['id'];
+    // (3) Buatkan perintah SQL DELETE untuk menghapus data dari tabel berdasarkan id mobil
+    $query = "DELETE FROM showroom_mobil WHERE id = $id";
+    // (4) Buatkan perkondisi jika eksekusi query berhasil
+    $result = mysqli_query($connect, $query);
+    if ($result) {
+        echo '<script>alert("Data Berhasil dihapus"); window.location.href = "list_mobil.php";</script>';
+    } else {
+        echo '<script>alert("Data Gagal dihapus"); window.location.href = "list_mobil.php";</script>';
+        echo "Error: " . mysqli_error($connect);
+    }
+}
+// Tutup koneksi kes database setelah selesai menggunakan database
+mysqli_close($connect);
+?>
